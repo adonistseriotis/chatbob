2c11e21246aad6f83e032893f5544c60a5ce99c5
adonistseriotis <adonis.tseriotis@gmail.com>
2021-05-19 22:44:36 +0300

    Terminal-like UI for communication with AI driven emotion detection software

---
+++ src/axiosConfig.jsx
@@ -0,0 +1,50 @@
+import Axios from 'axios';
+
+const axiosInstance = Axios.create({
+    baseURL: 'http://e06b7d5a7644.ngrok.io',
+    timeout: 5000,
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+    }
+});
+
+export async function run () {
+    const response = await axiosInstance
+        .get('/runquestionnaire')
+        .then(response => {
+            return response
+        })
+        .catch(error => {
+            throw error
+        })
+    console.log(response)
+    return response
+}
+
+export async function getquestion() {
+    const response = await axiosInstance
+        .post('/getquestion')
+        .then(response => {
+            return response.question
+        })
+        .catch(error => {
+            throw error
+        })
+    return response;
+}
+
+export async function answer(answer) {
+    const response = await axiosInstance
+    .post('/answer', { 'answer':answer })
+    .then(response => {
+        return response
+    })
+    .catch(error => {
+        throw error
+    })
+    return response
+}
+
+
+export default axiosInstance;
