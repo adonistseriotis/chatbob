e62a999ced32c1dba61f97db124b2a67904d9d63
adonistseriotis <adonis.tseriotis@gmail.com>
2021-05-27 15:29:14 +0300

    Deploy for demo

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,7 +1,7 @@
 import Axios from 'axios';
 
 const axiosInstance = Axios.create({
-    baseURL: 'http://c1cd883bb4a3.ngrok.io/',
+    baseURL: 'https://5656ba645f76.ngrok.io/',
     timeout: 5000,
     headers: {
         'Content-Type': 'application/json',
